a1a7bdccd9b8ac3c7c31c4814472459beb3cd9d9
Hasan Aytekin <aytekin.hasan.tahsin@gmail.com>
2023-01-07 17:40:35 +0300

    Test01 #2

--- main.cpp
+++ main.cpp
@@ -2,17 +2,19 @@
 #include <ranges>
 #include <vector>
 
+using namespace std;
+
 int main() {
-    using std::views::filter,
-            std::views::transform,
-            std::views::reverse; // Some data for us to work on
-    std::vector<int> numbers = {6, 5, 4, 3, 2, 1}; // Lambda function that will provide filtering
+    using views::filter,
+            views::transform,
+            views::reverse; // Some data for us to work on
+    vector<int> numbers = {6, 5, 4, 3, 2, 1}; // Lambda function that will provide filtering
     auto is_even = [](int n) { return n % 2 == 0; }; // Process our dataset
     auto results = numbers | filter(is_even)
                    | transform([](int n) { return n++; })
                    | reverse; // Use lazy evaluation to print out the results
     for (auto v: results) {
-        std::cout << v << " ";   // Output: 3 5 7
+        cout << v << " ";   // Output: 3 5 7
     }
     return 0;
 }
